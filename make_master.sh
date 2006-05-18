5a6f478d3d97da9bb07e3b5e022eaaf319d30035
proppy <proppy@c4f34931-41c7-4605-b615-9bb9f479c57e>
2006-05-18 01:14:54 +0000

    *** empty log message ***
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@1047 c4f34931-41c7-4605-b615-9bb9f479c57e

--- make_master.sh
+++ make_master.sh
@@ -1,93 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 BUILDDIR=$1
 MASTERDIR=$2
 MODULE=poker-eval
+CONFIGURATION=Release
 
-SRCDIR=$BUILDDIR/$MODULE
-DISTDIR=$MASTERDIR/$MODULE
-rm -fR $DISTDIR
-mkdir -p $DISTDIR
-HEADERS="\
- include/deck_undef.h\
- include/deck_std.h\
- include/deck_astud.h\
- include/rules_undef.h\
- include/combinations.h\
- include/deck_joker.h\
- include/deck.h\
- include/rules_astud.h\
- include/enumdefs.h\
- include/game_std.h\
- include/pokereval_export.h\
- include/game_astud.h\
- include/enumerate.h\
- include/handval.h\
- include/rules_joker.h\
- include/rules_std.h\
- include/enumord.h\
- include/game_joker.h\
- include/handval_low.h\
- include/evx_defs.h\
- include/poker_defs.h\
- include/inlines/eval_joker.h\
- include/inlines/eval_low27.h\
- include/inlines/eval_joker_low.h\
- include/inlines/evx_inlines.h\
- include/inlines/eval.h\
- include/inlines/evx5.h\
- include/inlines/evx7.h\
- include/inlines/evx_action.h\
- include/inlines/eval_omaha.h\
- include/inlines/eval_joker_low8.h\
- include/inlines/eval_low8.h\
- include/inlines/eval_astud.h\
- include/inlines/eval_type.h\
- include/inlines/eval_low.h"
+SRCDIR=${BUILDDIR}
+DISTDIR=${MASTERDIR}
+rm -fR ${DISTDIR}
+mkdir -p ${DISTDIR}
 
-cd $SRCDIR
-rsync -avR $HEADERS $DISTDIR/
-rsync -av include/poker_config.h.in $DISTDIR/include/poker_config.h
+rsync -avr ${SRCDIR}/include/ ${DISTDIR}/include/ --include='*.h' --include='inlines/' --include='inlines/*.h' --exclude='**'
+rsync -av ${SRCDIR}/include/poker_config.h.in ${DISTDIR}/include/poker_config.h
 
-LIBS=Release/poker-eval.lib
-rsync -av $LIBS $DISTDIR/lib/
+LIBS=${CONFIGURATION}/poker-eval.lib
+rsync -av ${SRCDIR}/${LIBS} ${DISTDIR}/lib/
 
 DOCS="\
- NEWS\
- README\
- TODO\
- AUTHORS\
- WHATS-HERE\
- debian/copyright"
+ ${SRCDIR}/NEWS\
+ ${SRCDIR}/README\
+ ${SRCDIR}/TODO\
+ ${SRCDIR}/AUTHORS\
+ ${SRCDIR}/WHATS-HERE\
+ ${SRCDIR}/debian/copyright"
 rsync -av $DOCS $DISTDIR/doc/
-rsync -av ChangeLog $DISTDIR/doc/changelog
+rsync -av ${SRCDIR}/ChangeLog $DISTDIR/doc/changelog
 
-EXAMPLES="\
- examples/fish.vcproj\
- examples/hcmp2.vcproj\
- examples/pokenum.vcproj\
- examples/pokenum.c\
- examples/usedecks.vcproj\
- examples/fish.c\
- examples/eval.c\
- examples/five_card_hands.c\
- examples/hcmp2.c\
- examples/hcmpn.c\
- examples/usedecks.c\
- examples/eval.vcproj\
- examples/seven_card_hands.vcproj\
- examples/hcmpn2.vcproj\
- examples/five_card_hands.vcproj\
- examples/getopt_w32.c\
- examples/getopt_w32.h\
- examples/seven_card_hands.c"
-rsync -av $EXAMPLES $DISTDIR/examples/
-
-NSI=poker-eval.nsi
-rsync -av $NSI $MASTERDIR
-
-export PATH=$PATH:/cygdrive/c/Program\ Files/NSIS
-cd $MASTERDIR
-makensis $NSI
-
-rm -f $NSI
+rsync -avr ${SRCDIR}/examples/ ${DISTDIR}/examples/ --include='*.vcproj' --include='*.c'
